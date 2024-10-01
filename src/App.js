1bf40dbf39c3309cc0cb410af22f62a9e589df44
Bhupendra <sainibhupendra50042@gmail.com>
2024-10-01 15:10:49 +0530

    Resolved merge conflicts

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
                 <div className="flex flex-col h-full">
                     <Header />
                     <Feed />
+                    {/* <VideoDetails /> */}
                     <Routes>
                         <Route path="/" element={<Feed />} />
                         <Route path="/searchResult/:searchQuery" element={<SearchResult />} />
